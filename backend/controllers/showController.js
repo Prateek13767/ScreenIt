d0987022fab310c6e0d35c05a61960a41bc04e9f
Prateek Soni <2023ucp1593@mnit.ac.in>
2025-06-28 19:34:31 +0530

    commit 9

---
+++ backend/controllers/showController.js
@@ -0,0 +1,13 @@
+
+export const getNowPlayingMovies=async (req,res)=>{
+    try {
+        const {data}=await axios.get('https://api.themoviedb.org/3/movie/nowplaying',{
+            headers:{Authorization:`Bearer ${process.env.TMDB_API_KEY}`}
+        })
+        const movies=data.results;
+        res.json({success:true,movies:movies})
+    } catch (error) {
+        console.log(error);
+        res.json({success:true,message:error.message});
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import {clerkMiddleware} from '@clerk/express';
 import {serve} from "inngest/express";
 import {inngest,functions} from './inngest/index.js'
+import showRouter from './routes/showRoutes.js';
 
 
 const app=express();
@@ -28,6 +29,7 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/api/inngest',serve({client:inngest,functions}))
+//app.use('/api/show',showRouter);
 
 
 
